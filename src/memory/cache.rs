06beb2ed33efff29a71f0e7b2b02a9b998d22e59
warhodes-dev <warhodes@gmail.com>
2022-09-19 13:01:21 -0400

    began work on DC cache

--- src/memory/cache.rs
+++ src/memory/cache.rs
@@ -1,11 +1,20 @@
 use crate::config;
 
-pub struct Cache {
-}
+pub type CacheEntry = (u32, u32);
+
+pub struct Cache(
+    Vec< // Sets
+        Vec< // Set Entries
+            Option<CacheEntry>>>); // Entry is either valid (Some) or invalid (None)
 
 impl Cache {
     pub fn new(cfg: &config::CacheConfig) -> Self {
-        Cache {
-        }
+        let empty_set = vec![ None ; cfg.set_entries ];
+        let cache_inner = vec![ empty_set ; cfg.sets ];
+        Cache(cache_inner)
+    }
+
+    pub fn lookup(&self, i: usize) {
+        
     }
 }
